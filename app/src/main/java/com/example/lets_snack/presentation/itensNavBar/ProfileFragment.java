2d21199cff4489a3931ffb37aa581c292b43af4e
arturnascimentosousa <artur.sousa@germinare.org.br>
2024-10-04 11:10:36 -0300

    adicionando tela de perfil

--- app/src/main/java/com/example/lets_snack/presentation/itensNavBar/ProfileFragment.java
+++ app/src/main/java/com/example/lets_snack/presentation/itensNavBar/ProfileFragment.java
@@ -7,9 +7,13 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.lets_snack.R;
 import com.example.lets_snack.databinding.FragmentProfileBinding;
 import com.example.lets_snack.presentation.login.LoginActivity;
+import com.example.lets_snack.presentation.transform.RoundedTransformation;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.squareup.picasso.Picasso;
 
 public class ProfileFragment extends Fragment {
 
@@ -24,14 +28,32 @@ public class ProfileFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout using View Binding
         binding = FragmentProfileBinding.inflate(inflater, container, false);
+
+        FirebaseUser currentUser = FirebaseAuth.getInstance().getCurrentUser();
+        if (currentUser != null) {
+            if (currentUser.getPhotoUrl() != null) {
+
+                Picasso.get()
+                        .load(currentUser.getPhotoUrl())
+                        .resize(300, 300)
+                        .centerCrop() // Centraliza e corta a imagem
+                        .placeholder(R.drawable.profile_default)
+                        .transform(new RoundedTransformation(180, 0) )
+                        .into(binding.profileImage);
+            } else {
+                binding.profileImage.setImageResource(R.drawable.profile_default);
+            }
+        }
+
         return binding.getRoot();
     }
 
     @Override
     public void onViewCreated(View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
         //fazendo logout
-        binding.exitBtn.setOnClickListener(new View.OnClickListener() {
+        binding.logoutBlock.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 FirebaseAuth.getInstance().signOut();
