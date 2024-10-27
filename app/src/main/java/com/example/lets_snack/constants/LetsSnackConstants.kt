f0f259e7117d30f930ca61d996ddf579906fee36
arturnascimentosousa <artur.sousa@germinare.org.br>
2024-10-27 16:10:25 -0300

    teste

--- app/src/main/java/com/example/lets_snack/constants/LetsSnackConstants.kt
+++ app/src/main/java/com/example/lets_snack/constants/LetsSnackConstants.kt
@@ -1,5 +1,6 @@
 package com.example.lets_snack.constants
 
 enum class LetsSnackConstants(val value: String) {
-    BASE_URL("https://jsonplaceholder.typicode.com/")
+    BASE_URL("https://jsonplaceholder.typicode.com/"),
+    TESTE("")
 }
